a0cd2d0f74680a94ba4bc96dca817dbaf5064876
BrennDev <134910223+BrennDev@users.noreply.github.com>
2023-10-16 12:23:34 -0600

    Estilo a la barra de navegación utilizando bootstrap

---
+++ src/components/AppBar/AppBar.tsx
@@ -0,0 +1,29 @@
+import logo from "../../assets/hamburguesa.png"
+import { Navbar, Container } from 'react-bootstrap'
+
+export default function AppBar() {
+  return (
+    <>
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home"></Navbar.Brand>
+        <img
+            alt=""
+            src={logo}
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+          />{' '}
+          <Navbar.Text style={{fontSize:40}} >Burger Queen</Navbar.Text>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Mark Otto</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    
+  </>
+  )
+}
--- src/components/Navigate/navigate.tsx
+++ src/components/Navigate/navigate.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 
-export default function NavigateTo(path){
+export default function NavigateTo(path: string){
     const navigate = useNavigate();
 
     const handleClick = () => {
