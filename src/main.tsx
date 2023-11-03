ab296967fd12b07384dfe028496fc0dd385d95c2
Natali <natalic1008.nm@gmail.com>
2023-11-03 12:12:32 -0500

    Se agrego todas las vistas con las que se necesita interactuar

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,11 @@ import OrderList from './pages/Waiter/OrdersList.tsx';
 import App from './App.tsx';
 import Login from './pages/Login/Login.tsx';
 import OrderHistory from './pages/Waiter/OrderHistory.tsx';
+import Dashboard from './pages/Dashboard.tsx';
+import Products from './pages/Admin/Products.tsx';
+import Users from './pages/Admin/Users.tsx';
+import AllOrders from './pages/Admin/AllOrders.tsx';
+import ActiveOrders from './pages/Cheff/ActiveOrders.tsx';
 
 
 const router = createBrowserRouter([
@@ -14,10 +19,35 @@ const router = createBrowserRouter([
     element:<Login/>,
   },
   {
+    path: "/admin",
+    element: <App />,
+    children: [
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "users",
+        element: <Users />,
+      },
+      {
+        path: "orders",
+        element: <AllOrders />,
+      },
+    ],
+  },
+  {
     path: "/waiter",
     element: <App/>,
     children: [
-      
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
       {
         path: "new",
         element: <OrderList />,
@@ -27,7 +57,21 @@ const router = createBrowserRouter([
         element:<OrderHistory/>
       }
     ]
-  }
+  },
+  {
+    path: "/chef",
+    element: <App />,
+    children: [
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "orders",
+        element: <ActiveOrders/>,
+      }
+    ],
+  },
 
 ])
 
