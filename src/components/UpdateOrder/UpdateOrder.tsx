56bf11e95a2a0eacb4b866dd1f7de5a2f22b7f88
Natali <natalic1008.nm@gmail.com>
2023-12-12 10:45:25 -0500

    Se agrego estilo a la vista de Editar Ordenes, la interacción entre mesero-chef y administrador esta lista

--- src/components/UpdateOrder/UpdateOrder.tsx
+++ src/components/UpdateOrder/UpdateOrder.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import CheckOut from "../components/CheckOut/CheckOut";
-
+import CheckOut from "../CheckOut/CheckOut";
+import style from "./UpdateOrder.module.css";
+import { Button } from "react-bootstrap";
 
 type Item = {
   id: number;
@@ -92,8 +93,8 @@ export default function UpdateOrder({
   
 
   return (
-    <section>
-      <div >
+    <section className={style.order}>
+      <div className={style.header}>
         <h1>Order</h1>
         <select
           value={tableNumber}
@@ -102,14 +103,14 @@ export default function UpdateOrder({
           {options}
         </select>
       </div>
-      <div >
+      <div className={style.items} >
       {orderInfo && orderInfo.products && orderInfo.products.length === 0 ? (
-          <div >Loading</div>
+          <div className={style.emptyBasket}>Loading</div>
         ) : (          
           orderInfo &&
           Array.isArray(orderInfo.products) &&
           orderInfo.products.map((item: Product, index: number) => (
-            <div key={index} >
+            <div key={index} className={style.item} >
               <div>
               {isProduct(item) && (
                 <>
@@ -119,21 +120,21 @@ export default function UpdateOrder({
               )}
 
               </div>
-              <div>
-                <button onClick={() => handleAddToSelectedItems(item)}>+</button>
-                <span >{isProduct(item) ? item.qty : ''}</span>
-                <button onClick={() => handleRemoveSelectedItems(item)}>-</button>
+              <div className={style.container}>
+                <Button  className={style.add_item} onClick={() => handleAddToSelectedItems(item)}>+</Button>
+                <span className={style.qty}>{isProduct(item) ? item.quantity : ''}</span>
+                <Button className={style.reduce_item} onClick={() => handleRemoveSelectedItems(item)}>-</Button>
               </div>
             </div>
           ))
         )}
       </div>
-      <div >
+      <div  className={style.items}>
         {selectedItems.length === 0 && !orderInfo ? (
-          <div >No items selected</div>
+          <div className={style.emptyBasket} >No items selected</div>
         ) : (
           selectedItems.map((item, index) => (
-            <div key={index}>
+            <div key={index} className={style.item}>
               <div>
               {isProduct(item) ? (
                   <>
@@ -147,24 +148,24 @@ export default function UpdateOrder({
                   )}                
 
               </div>
-              <div >
-                <button onClick={() => handleAddToSelectedItems(item as Product)}>+</button>
-                <span >{isProduct(item) ? item.qty : ''}</span>
-                <button onClick={() => handleRemoveSelectedItems(item as Product)}>-</button>
+              <div className={style.container}>
+                <Button className={style.add_item} onClick={() => handleAddToSelectedItems(item as Product)}>+</Button>
+                <span className={style.qty}>{isProduct(item) ? item.quantity : ''}</span>
+                <Button className={style.reduce_item} onClick={() => handleRemoveSelectedItems(item as Product)}>-</Button>
               </div>
             </div>
           ))
         )}
       </div>
-      <div >
-        <div >
+      <div className={style.buttons} >
+        <div className={style.price}>
           <span>Total</span>
-          <span >${calculateTotal()}</span>
+          <span className={style.total}>${calculateTotal()}</span>
         </div>
-        <button onClick={handleButtonClick}>
+        <Button  className={style.button_send} onClick={handleButtonClick}>
           {orderInfo ? "Update Order" : "Send"}
-        </button>
-        <button onClick={handleOpenModal}>Check Out</button>
+        </Button>
+        <Button className={style.button} onClick={handleOpenModal}>Check Out</Button>
         {showModal && (
           <CheckOut
             onClose={handleCloseModal}
